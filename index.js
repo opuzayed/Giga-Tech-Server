6698cee8ed4811f21273104b823fd67246bcb5be
opuzayed <zayedopu4933@gmail.com>
2022-10-28 12:06:49 +0600

    gigaTech server data done

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
 
-/* app.get('/category/:id', (req, res) => {
+app.get('/category/:id', (req, res) => {
     const id = req.params.id;
         const category_news = news.filter(n => n.category_id === id);
         res.send(category_news);
     
-}) */
+})
 
 app.get('/news', (req, res) =>{
     res.send(news);
