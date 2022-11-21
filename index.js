2e393ca6533e111ad43715a754adec9775e6a408
opuzayed <zayedopu4933@gmail.com>
2022-11-21 13:38:58 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json'); 
-const news = require('./data/Course.json');
+const categories = require("./data/categories.json");
+const news = require("./data/Course.json");
 
-app.get('/', (req, res) => {
-    res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("Course api Running");
 });
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories)
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
 });
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    
-})
-
-app.get('/news', (req, res) =>{
-    res.send(news);
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_news = news.filter((n) => n.category_id === id);
+  res.send(category_news);
 });
 
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
-    console.log('Course Server running on port', port);
-})
+  console.log("Course Server running on port", port);
+});
